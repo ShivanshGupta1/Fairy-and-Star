65d536dc26dd23a7d54ca9b432b6816faef9f517
Shivansh Gupta <mail@shivanshgupta.com>
2021-03-13 13:19:26 +0530

    Done

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 var starImg, fairyImg, bgImg;
-var fairy , fairyVoice;
+var fairy, fairyVoice;
 var star, starBody;
 
 const Engine = Matter.Engine;
@@ -7,34 +7,33 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
-function preload()
-{
+function preload() {
 	starImg = loadImage("images/star.png");
-	fairyImg = loadAnimation("images/fairyImage1.png","images/fairyImage2.png");
+	fairyImg = loadAnimation("images/fairyImage1.png", "images/fairyImage2.png");
 	bgImg = loadImage("images/starNight.png");
 	fairyVoice = loadSound("sound/JoyMusic.mp3");
 
 }
 
 function setup() {
-	createCanvas(800, 750);
+	createCanvas(800, 600);
 
 	// fairyVoice.play();
 
 	fairy = createSprite(130, 520);
-	fairy.addAnimation("fairyflying",fairyImg);  
-	fairy.scale =0.25;
+	fairy.addAnimation("fairyflying", fairyImg);
+	fairy.scale = 0.25;
 
-	star = createSprite(650,30);
+	star = createSprite(650, 30);
 	star.addImage(starImg);
 	star.scale = 0.2;
 
 	engine = Engine.create();
 	world = engine.world;
 
-	starBody = Bodies.circle(650 , 30 , 5 , {restitution:0.5, isStatic:false});
+	starBody = Bodies.circle(650, 30, 5, { restitution: 0.5, isStatic: true });
 	World.add(world, starBody);
-	
+
 	Engine.run(engine);
 
 }
@@ -42,26 +41,25 @@ function setup() {
 
 function draw() {
 	Engine.update(engine);
-  background(bgImg);
-  drawSprites();
-  
- if (keyDown(RIGHT_ARROW)){
-	fairy.x +=5
-  }
-if (keyDown(LEFT_ARROW)){
-	fairy.x -=5
-  }
+	background(bgImg);
+	drawSprites();
 
-
-  if (starBody.position.y>470){
-  Matter.Body.setStatic(starBody, true);
-  }
-  keyPressed();
+	if (keyDown(RIGHT_ARROW)) {
+		fairy.x += 5
+	}
+	if (keyDown(LEFT_ARROW)) {
+		fairy.x -= 5
+	}
+	if (star.isTouching(fairy)) {
+		Body.setStatic(starBody, true);
+	}
+	star.x = starBody.position.x
+	star.y = starBody.position.y
+	keyPressed(); 
 }
 
 function keyPressed() {
-	if (keyDown(DOWN_ARROW)){
-	star.x = starBody.position.x
-	star.y = starBody.position.y
-	}
+	if (keyDown(DOWN_ARROW)) {
+		Body.setStatic(starBody, false);
+		} 
 }
